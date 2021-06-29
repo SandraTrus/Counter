747c305484deebc074bac0a6445074a81a26e699
unknown <you@example.com>
2021-06-29 23:44:35 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    String five = "5";
+    String eight = "8";
+    int sum = Integer.parseInt(five) + Integer.parseInt(eight);
+        System.out.println(sum);
+    }
+}
